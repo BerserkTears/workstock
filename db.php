262e01782d8bbc2192a286c07b533124dd5e16de
BerserkTears <80447364+BerserkTears@users.noreply.github.com>
2021-04-05 13:18:17 +0900

    upd db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php 
 require "libs/rb.php";
-R::setup( 'mysql:host=localhost;dbname=u573813566_workstock', 'u573813566_workstock', 'Workstock1' );
+R::setup( 'mysql:host=localhost;dbname=dbname', 'username', 'password' );
 session_start();
 ?>
